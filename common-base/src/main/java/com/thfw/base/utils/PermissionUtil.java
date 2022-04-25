683121db5f69d260113f11635334cfd56803645a
DomerJi <pengshuaiji@outlook.com>
2022-04-25 13:52:40 +0800

    修复权限申请bug，登录禁止输入空格，换行

---
+++ common-base/src/main/java/com/thfw/base/utils/PermissionUtil.java
@@ -0,0 +1,6 @@
+package com.thfw.base.utils;/**
+ * Author:pengs
+ * Date: 2022/4/25 9:20
+ * Describe:Todo
+ */public class PermissionUtil {
+}
---
+++ common-ui/src/main/java/com/thfw/ui/utils/EditTextUtil.java
@@ -0,0 +1,6 @@
+package com.thfw.ui.utils;/**
+ * Author:pengs
+ * Date: 2022/4/25 11:37
+ * Describe:Todo
+ */public class EditTextUtil {
+}
