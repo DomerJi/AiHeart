e73010365bb3f8651fcd618fd99793d7b7f1da87
DomerJi <pengshuaiji@outlook.com>
2022-05-17 09:21:40 +0800

    webview size 大小缩放区别平板和手机

--- app/src/main/java/com/thfw/mobileheart/util/WebSizeUtil.java
+++ app/src/main/java/com/thfw/mobileheart/util/WebSizeUtil.java
@@ -5,6 +5,7 @@ import android.util.DisplayMetrics;
 import android.webkit.WebSettings;
 
 import com.thfw.base.utils.LogUtil;
+import com.thfw.base.utils.Util;
 
 /**
  * Author:pengs
@@ -16,6 +17,15 @@ public class WebSizeUtil {
     public static void setSize(Context context, WebSettings webSettings) {
         int screenDensity = context.getResources().getDisplayMetrics().densityDpi;
         float lv = screenDensity * 1.0f / DisplayMetrics.DENSITY_220;
+        if (Util.isPad(context)) {
+            if (lv < 1.45f) {
+                lv = 1.45f;
+            }
+        } else {
+            if (lv < 2.18f) {
+                lv = 2.2f;
+            }
+        }
         int sizeZoom = (int) (lv * 100f);
         LogUtil.d("WebSizeUtil", "screenDensity = " + screenDensity);
         LogUtil.d("WebSizeUtil", "lv = " + lv);
@@ -27,6 +37,16 @@ public class WebSizeUtil {
     public static void setSizeByAgree(Context context, WebSettings webSettings) {
         int screenDensity = context.getResources().getDisplayMetrics().densityDpi;
         float lv = screenDensity * 1.0f / DisplayMetrics.DENSITY_MEDIUM;
+        if (Util.isPad(context)) {
+            if (lv < 2.0f) {
+                lv = 2.0f;
+            }
+        } else {
+            if (lv < 3.0f) {
+                lv = 3.0f;
+            }
+        }
+
         int sizeZoom = (int) (lv * 100f);
         LogUtil.d("WebSizeUtil", "screenDensity = " + screenDensity);
         LogUtil.d("WebSizeUtil", "lv = " + lv);
