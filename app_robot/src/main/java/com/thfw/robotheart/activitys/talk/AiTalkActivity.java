9e2a8df9f1da475764cf85e16e35fd488ebbc331
DomerJi <pengshuaiji@outlook.com>
2022-08-29 11:37:54 +0800

    唱歌正则优化

--- app_robot/src/main/java/com/thfw/robotheart/activitys/talk/AiTalkActivity.java
+++ app_robot/src/main/java/com/thfw/robotheart/activitys/talk/AiTalkActivity.java
@@ -487,8 +487,7 @@ public class AiTalkActivity extends RobotBaseActivity<TalkPresenter> implements 
         if (inputText.contains("冥想")) {
             return false;
         }
-        String REGEX_MUSIC = ".{0,5}(播放|推荐|放|唱|听|来)(一个|一首|一曲|个|首|).{0,20}" +
-                "(的音乐|的歌曲|歌儿|歌吧|的歌|歌曲|音乐|曲儿|个歌|歌|music)(吧|嘛|)";
+        String REGEX_MUSIC = ".{0,5}(播放|推荐|放|唱|听|来)(一个|一首|一曲|个|首|).{0,20}";
         if (tempText.matches(REGEX_MUSIC)) {
             String REGEX_MUSIC_REPLACE = ".{0,5}(播放|推荐|放|唱|听|来)(一个|一首|一曲|个|首|)" +
                     "|(的音乐|的歌曲|歌儿|歌吧|的歌|歌曲|音乐|曲儿|个歌|music)(吧|嘛|)";
