4c9f4e52bdd1415f74e47063d2a36b38ad4be574
DomerJi <pengshuaiji@outlook.com>
2022-09-28 09:30:54 +0800

    百科名字1-8修改

--- app_robot/src/main/java/com/thfw/robotheart/activitys/talk/AiTalkActivity.java
+++ app_robot/src/main/java/com/thfw/robotheart/activitys/talk/AiTalkActivity.java
@@ -795,10 +795,10 @@ public class AiTalkActivity extends RobotBaseActivity<TalkPresenter> implements 
 
         String tempText = inputText;
 
-        String regexAsk = ".{1,5}(是)(谁|干什么用的|干什么的|干啥的|啥|什么东西|什么|干啥用的|做什么用的)(呀|)";
-        String regexAsk2 = "(你|)(认识|知道).{1,5}(吗)";
-        String regexAsk3 = "(给我|给|请|)(介绍一下).{1,5}";
-        String regexAsk4 = "(把).{1,5}(介绍一下)(吧|)";
+        String regexAsk = ".{1,8}(是)(谁|干什么用的|干什么的|干啥的|啥|什么东西|什么|干啥用的|做什么用的)(呀|)";
+        String regexAsk2 = "(你|)(认识|知道).{1,8}(吗)";
+        String regexAsk3 = "(给我|给|请|)(介绍一下).{1,8}";
+        String regexAsk4 = "(把).{1,8}(介绍一下)(吧|)";
 
         if (tempText.matches(regexAsk) || tempText.matches(regexAsk2)
                 || tempText.matches(regexAsk3) || tempText.matches(regexAsk4)) {
